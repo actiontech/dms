f55d5037a763ff84e1fcfc20e8f972da916cff00
rocky114 <934313141@qq.com>
2023-09-13 16:12:58 +0800

    fix: typo

--- internal/dms/biz/basic_ce.go
+++ internal/dms/biz/basic_ce.go
@@ -7,12 +7,12 @@ import (
 	"errors"
 )
 
-var errNotBasicConfig = errors.New("personalisation are enterprise version functions")
+var errNotBasicConfig = errors.New("personalization are enterprise version functions")
 
 func (d *BasicUsecase) GetStaticLogo(ctx context.Context) (*BasicConfigParams, string, error) {
 	return nil, "", errNotBasicConfig
 }
 
-func (d *BasicUsecase) Personalisation(ctx context.Context, params *BasicConfigParams) error {
+func (d *BasicUsecase) Personalization(ctx context.Context, params *BasicConfigParams) error {
 	return errNotBasicConfig
 }
