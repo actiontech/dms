39971f3eb1f298fce38487e97169aec9aaa80702
yangzhongjiao <yangzhongjiao@actionsky.com>
2023-12-28 03:34:18 +0000

    fix: limit structure column length

--- internal/dms/storage/model/model.go
+++ internal/dms/storage/model/model.go
@@ -41,7 +41,7 @@ func GetAllModels() []interface{} {
 }
 
 type Model struct {
-	UID       string    `json:"uid" gorm:"primaryKey" example:"1"`
+	UID       string    `json:"uid" gorm:"primaryKey;size:32" example:"1"`
 	CreatedAt time.Time `json:"created_at" example:"2018-10-21T16:40:23+08:00"`
 	UpdatedAt time.Time `json:"updated_at" example:"2018-10-21T16:40:23+08:00"`
 }
@@ -49,16 +49,16 @@ type Model struct {
 type DBService struct {
 	Model
 	Name              string          `json:"name" gorm:"size:200;not null;index:project_uid_name,unique" example:""`
-	DBType            string          `json:"db_type" gorm:"column:db_type; not null" example:"mysql"`
-	Host              string          `json:"host" gorm:"column:db_host; not null" example:"10.10.10.10"`
-	Port              string          `json:"port" gorm:"column:db_port; not null" example:"3306"`
-	User              string          `json:"user" gorm:"column:db_user; not null" example:"root"`
-	Password          string          `json:"password" gorm:"column:db_password; not null"`
+	DBType            string          `json:"db_type" gorm:"size:255;column:db_type; not null" example:"mysql"`
+	Host              string          `json:"host" gorm:"column:db_host;size:255; not null" example:"10.10.10.10"`
+	Port              string          `json:"port" gorm:"column:db_port;size:255; not null" example:"3306"`
+	User              string          `json:"user" gorm:"column:db_user;size:255; not null" example:"root"`
+	Password          string          `json:"password" gorm:"column:db_password; size:255; not null"`
 	Desc              string          `json:"desc" gorm:"column:desc" example:"this is a instance"`
-	Business          string          `json:"business" gorm:"column:business; not null" example:"this is a business"`
+	Business          string          `json:"business" gorm:"column:business;size:255; not null" example:"this is a business"`
 	AdditionalParams  params.Params   `json:"additional_params" gorm:"type:text"`
-	Source            string          `json:"source" gorm:"not null"`
-	ProjectUID        string          `json:"project_uid" gorm:"column:project_uid;index:project_uid_name,unique"`
+	Source            string          `json:"source" gorm:"size:255;not null"`
+	ProjectUID        string          `json:"project_uid" gorm:"size:32;column:project_uid;index:project_uid_name,unique"`
 	MaintenancePeriod periods.Periods `json:"maintenance_period" gorm:"type:text"`
 	ExtraParameters   ExtraParameters `json:"extra_parameters" gorm:"TYPE:json"`
 }
@@ -92,12 +92,12 @@ type SqlQueryConfig struct {
 type User struct {
 	Model
 	Name                   string         `json:"name" gorm:"size:200;column:name"`
-	ThirdPartyUserID       string         `json:"third_party_user_id" gorm:"third_party_user_id;column:third_party_user_id"`
-	Email                  string         `json:"email" gorm:"column:email"`
-	Phone                  string         `json:"phone" gorm:"column:phone"`
-	WeChatID               string         `json:"wechat_id" gorm:"column:wechat_id"`
-	Password               string         `json:"password" gorm:"column:password"`
-	UserAuthenticationType string         `json:"user_authentication_type" gorm:"not null;column:user_authentication_type"`
+	ThirdPartyUserID       string         `json:"third_party_user_id" gorm:"size:255;third_party_user_id;column:third_party_user_id"`
+	Email                  string         `json:"email" gorm:"size:255;column:email"`
+	Phone                  string         `json:"phone" gorm:"size:255;column:phone"`
+	WeChatID               string         `json:"wechat_id" gorm:"size:255;column:wechat_id"`
+	Password               string         `json:"password" gorm:"size:255;column:password"`
+	UserAuthenticationType string         `json:"user_authentication_type" gorm:"size:255;not null;column:user_authentication_type"`
 	Stat                   uint           `json:"stat" gorm:"not null"`
 	LastLoginAt            *time.Time     `json:"last_login_at" gorm:"column:last_login_at"`
 	DeletedAt              gorm.DeletedAt `json:"delete_at" gorm:"column:delete_at" sql:"index"`
@@ -110,7 +110,7 @@ type UserGroup struct {
 	Model
 	Name string `json:"name" gorm:"size:200;uniqueIndex"`
 	Desc string `json:"desc" gorm:"column:description"`
-	Stat uint   `json:"stat" gorm:"not null"`
+	Stat uint   `json:"stat" gorm:"size:32;not null"`
 
 	Users []*User `gorm:"many2many:user_group_users"`
 }
@@ -119,7 +119,7 @@ type Role struct {
 	Model
 	Name string `json:"name" gorm:"size:200;uniqueIndex"`
 	Desc string `json:"desc" gorm:"column:description"`
-	Stat uint   `json:"stat" gorm:"not null"`
+	Stat uint   `json:"stat" gorm:"size:32;not null"`
 
 	OpPermissions []*OpPermission `gorm:"many2many:role_op_permissions"`
 }
@@ -128,7 +128,7 @@ type OpPermission struct {
 	Model
 	Name      string `json:"name" gorm:"size:200;uniqueIndex"`
 	Desc      string `json:"desc" gorm:"column:description"`
-	RangeType string `json:"range_type" gorm:"column:range_type"`
+	RangeType string `json:"range_type" gorm:"size:255;column:range_type"`
 }
 
 type DMSConfig struct {
@@ -141,15 +141,15 @@ type DMSConfig struct {
 
 type Member struct {
 	Model
-	UserUID          string              `json:"user_uid" gorm:"column:user_uid;index:project_user_id,unique"`
-	ProjectUID       string              `json:"project_uid" gorm:"column:project_uid;index:project_user_id,unique"`
+	UserUID          string              `json:"user_uid" gorm:"size:32;column:user_uid;index:project_user_id,unique"`
+	ProjectUID       string              `json:"project_uid" gorm:"size:32;column:project_uid;index:project_user_id,unique"`
 	RoleWithOpRanges []MemberRoleOpRange `json:"role_with_op_ranges" gorm:"foreignKey:MemberUID;references:UID"`
 }
 
 type MemberRoleOpRange struct {
-	MemberUID   string `json:"member_uid" gorm:"size:200;column:member_uid"`
-	RoleUID     string `json:"role_uid" gorm:"column:role_uid"`
-	OpRangeType string `json:"op_range_type" gorm:"column:op_range_type"`
+	MemberUID   string `json:"member_uid" gorm:"size:32;column:member_uid"`
+	RoleUID     string `json:"role_uid" gorm:"size:32;column:role_uid"`
+	OpRangeType string `json:"op_range_type" gorm:"size:255;column:op_range_type"`
 	RangeUIDs   string `json:"range_uids" gorm:"type:text;column:range_uids"`
 }
 
@@ -160,15 +160,15 @@ func (mg *MemberRoleOpRange) AfterSave(tx *gorm.DB) error {
 type MemberGroup struct {
 	Model
 	Name             string                   `json:"name" gorm:"size:200;index:project_uid_name,unique"`
-	ProjectUID       string                   `json:"project_uid" gorm:"column:project_uid;index:project_uid_name,unique"`
+	ProjectUID       string                   `json:"project_uid" gorm:"size:32;column:project_uid;index:project_uid_name,unique"`
 	Users            []*User                  `gorm:"many2many:member_group_users"`
 	RoleWithOpRanges []MemberGroupRoleOpRange `json:"role_with_op_ranges" gorm:"foreignKey:MemberGroupUID;references:UID"`
 }
 
 type MemberGroupRoleOpRange struct {
-	MemberGroupUID string `json:"member_group_uid" gorm:"size:200;column:member_group_uid"`
-	RoleUID        string `json:"role_uid" gorm:"column:role_uid"`
-	OpRangeType    string `json:"op_range_type" gorm:"column:op_range_type"`
+	MemberGroupUID string `json:"member_group_uid" gorm:"size:32;column:member_group_uid"`
+	RoleUID        string `json:"role_uid" gorm:"size:32;column:role_uid"`
+	OpRangeType    string `json:"op_range_type" gorm:"size:255;column:op_range_type"`
 	RangeUIDs      string `json:"range_uids" gorm:"type:text;column:range_uids"`
 }
 
@@ -178,43 +178,43 @@ func (mg *MemberGroupRoleOpRange) AfterSave(tx *gorm.DB) error {
 
 type Project struct {
 	Model
-	Name          string `json:"name" gorm:"column:name;index:name,unique"`
+	Name          string `json:"name" gorm:"size:200;column:name;index:name,unique"`
 	Desc          string `json:"desc" gorm:"column:desc"`
-	CreateUserUID string `json:"create_user_uid" gorm:"column:create_user_uid"`
-	Status        string `gorm:"default:'active'"`
+	CreateUserUID string `json:"create_user_uid" gorm:"size:32;column:create_user_uid"`
+	Status        string `gorm:"size:64;default:'active'"`
 }
 
 type ProxyTarget struct {
 	Name            string `json:"name" gorm:"primaryKey;size:200;not null;column:name"`
-	Url             string `json:"url" gorm:"column:url"`
-	Version         string `json:"version" gorm:"column:version"`
-	ProxyUrlPrefixs string `json:"proxy_url_prefixs" gorm:"column:proxy_url_prefixs"`
+	Url             string `json:"url" gorm:"size:255;column:url"`
+	Version         string `json:"version" gorm:"size:64;column:version"`
+	ProxyUrlPrefixs string `json:"proxy_url_prefixs" gorm:"size:255;column:proxy_url_prefixs"`
 }
 
 type Plugin struct {
 	Name                         string `json:"name" gorm:"primaryKey;size:200;not null;column:name"`
-	AddDBServicePreCheckUrl      string `json:"add_db_service_pre_check_url" gorm:"column:add_db_service_pre_check_url"`
-	DelDBServicePreCheckUrl      string `json:"del_db_service_pre_check_url" gorm:"column:del_db_service_pre_check_url"`
-	DelUserPreCheckUrl           string `json:"del_user_pre_check_url" gorm:"column:del_user_pre_check_url"`
-	DelUserGroupPreCheckUrl      string `json:"del_user_group_pre_check_url" gorm:"column:del_user_group_pre_check_url"`
-	OperateDataResourceHandleUrl string `json:"operate_data_resource_handle_url" gorm:"column:operate_data_resource_handle_url"`
+	AddDBServicePreCheckUrl      string `json:"add_db_service_pre_check_url" gorm:"size:255;column:add_db_service_pre_check_url"`
+	DelDBServicePreCheckUrl      string `json:"del_db_service_pre_check_url" gorm:"size:255;column:del_db_service_pre_check_url"`
+	DelUserPreCheckUrl           string `json:"del_user_pre_check_url" gorm:"size:255;column:del_user_pre_check_url"`
+	DelUserGroupPreCheckUrl      string `json:"del_user_group_pre_check_url" gorm:"size:255;column:del_user_group_pre_check_url"`
+	OperateDataResourceHandleUrl string `json:"operate_data_resource_handle_url" gorm:"size:255;column:operate_data_resource_handle_url"`
 }
 
 // Oauth2Configuration store oauth2 server configuration.
 type Oauth2Configuration struct {
 	Model
 	EnableOauth2    bool   `json:"enable_oauth2" gorm:"column:enable_oauth2"`
-	ClientID        string `json:"client_id" gorm:"column:client_id"`
+	ClientID        string `json:"client_id" gorm:"size:255;column:client_id"`
 	ClientKey       string `json:"-" gorm:"-"`
-	ClientSecret    string `json:"client_secret" gorm:"client_secret"`
-	ClientHost      string `json:"client_host" gorm:"column:client_host"`
-	ServerAuthUrl   string `json:"server_auth_url" gorm:"column:server_auth_url"`
-	ServerTokenUrl  string `json:"server_token_url" gorm:"column:server_token_url"`
-	ServerUserIdUrl string `json:"server_user_id_url" gorm:"column:server_user_id_url"`
-	Scopes          string `json:"scopes" gorm:"column:scopes"`
-	AccessTokenTag  string `json:"access_token_tag" gorm:"column:access_token_tag"`
-	UserIdTag       string `json:"user_id_tag" gorm:"column:user_id_tag"`
-	LoginTip        string `json:"login_tip" gorm:"column:login_tip; default:'使用第三方账户登录'"`
+	ClientSecret    string `json:"client_secret" gorm:"size:255;client_secret"`
+	ClientHost      string `json:"client_host" gorm:"size:255;column:client_host"`
+	ServerAuthUrl   string `json:"server_auth_url" gorm:"size:255;column:server_auth_url"`
+	ServerTokenUrl  string `json:"server_token_url" gorm:"size:255;column:server_token_url"`
+	ServerUserIdUrl string `json:"server_user_id_url" gorm:"size:255;column:server_user_id_url"`
+	Scopes          string `json:"scopes" gorm:"size:255;column:scopes"`
+	AccessTokenTag  string `json:"access_token_tag" gorm:"size:255;column:access_token_tag"`
+	UserIdTag       string `json:"user_id_tag" gorm:"size:255;column:user_id_tag"`
+	LoginTip        string `json:"login_tip" gorm:"size:255;column:login_tip; default:'使用第三方账户登录'"`
 }
 
 // LDAPConfiguration store ldap server configuration.
@@ -225,29 +225,29 @@ type LDAPConfiguration struct {
 	// whether the ssl is enabled
 	EnableSSL bool `json:"enable_ssl" gorm:"not null"`
 	// ldap server's ip
-	Host string `json:"host" gorm:"not null"`
+	Host string `json:"host" gorm:"size:255;not null"`
 	// ldap server's port
-	Port string `json:"port" gorm:"not null"`
+	Port string `json:"port" gorm:"size:255;not null"`
 	// the DN of the ldap administrative user for verification
-	ConnectDn string `json:"connect_dn" gorm:"not null"`
+	ConnectDn string `json:"connect_dn" gorm:"size:255;not null"`
 	// the secret password of the ldap administrative user for verification
-	ConnectSecretPassword string `json:"connect_secret_password" gorm:"not null"`
+	ConnectSecretPassword string `json:"connect_secret_password" gorm:"size:255;not null"`
 	// base dn used for ldap verification
-	BaseDn string `json:"base_dn" gorm:"not null"`
+	BaseDn string `json:"base_dn" gorm:"size:255;not null"`
 	// the key corresponding to the user name in ldap
-	UserNameRdnKey string `json:"ldap_user_name_rdn_key" gorm:"not null"`
+	UserNameRdnKey string `json:"ldap_user_name_rdn_key" gorm:"size:255;not null"`
 	// the key corresponding to the user email in ldap
-	UserEmailRdnKey string `json:"ldap_user_email_rdn_key" gorm:"not null"`
+	UserEmailRdnKey string `json:"ldap_user_email_rdn_key" gorm:"size:255;not null"`
 }
 
 // SMTPConfiguration store SMTP server configuration.
 type SMTPConfiguration struct {
 	Model
 	EnableSMTPNotify bool   `json:"enable_smtp_notify" gorm:"default:true"`
-	Host             string `json:"smtp_host" gorm:"column:smtp_host; not null"`
-	Port             string `json:"smtp_port" gorm:"column:smtp_port; not null"`
-	Username         string `json:"smtp_username" gorm:"column:smtp_username; not null"`
-	SecretPassword   string `json:"secret_smtp_password" gorm:"column:secret_smtp_password; not null"`
+	Host             string `json:"smtp_host" gorm:"size:255;column:smtp_host; not null"`
+	Port             string `json:"smtp_port" gorm:"size:255;column:smtp_port; not null"`
+	Username         string `json:"smtp_username" gorm:"size:255;column:smtp_username; not null"`
+	SecretPassword   string `json:"secret_smtp_password" gorm:"size:255;column:secret_smtp_password; not null"`
 	IsSkipVerify     bool   `json:"is_skip_verify" gorm:"default:false; not null"`
 }
 
@@ -255,11 +255,11 @@ type SMTPConfiguration struct {
 type WeChatConfiguration struct {
 	Model
 	EnableWeChatNotify  bool   `json:"enable_wechat_notify" gorm:"not null"`
-	CorpID              string `json:"corp_id" gorm:"not null"`
-	EncryptedCorpSecret string `json:"encrypted_corp_secret" gorm:"not null"`
+	CorpID              string `json:"corp_id" gorm:"size:255;not null"`
+	EncryptedCorpSecret string `json:"encrypted_corp_secret" gorm:"size:255;not null"`
 	AgentID             int    `json:"agent_id" gorm:"not null"`
 	SafeEnabled         bool   `json:"safe_enabled" gorm:"not null"`
-	ProxyIP             string `json:"proxy_ip"`
+	ProxyIP             string `json:"proxy_ip" gorm:"size:255"`
 }
 
 type WebHookConfiguration struct {
@@ -267,8 +267,8 @@ type WebHookConfiguration struct {
 	Enable               bool   `json:"enable" gorm:"default:true;not null"`
 	MaxRetryTimes        int    `json:"max_retry_times" gorm:"not null"`
 	RetryIntervalSeconds int    `json:"retry_interval_seconds" gorm:"not null"`
-	EncryptedToken       string `json:"encrypted_token" gorm:"not null"`
-	URL                  string `json:"url" gorm:"not null"`
+	EncryptedToken       string `json:"encrypted_token" gorm:"size:255;not null"`
+	URL                  string `json:"url" gorm:"size:255;not null"`
 }
 
 const (
@@ -279,36 +279,36 @@ const (
 // Instant messaging config
 type IMConfiguration struct {
 	Model
-	AppKey      string `json:"app_key" gorm:"column:app_key"`
-	AppSecret   string `json:"app_secret" gorm:"column:app_secret"`
+	AppKey      string `json:"app_key" gorm:"size:255;column:app_key"`
+	AppSecret   string `json:"app_secret" gorm:"size:255;column:app_secret"`
 	IsEnable    bool   `json:"is_enable" gorm:"column:is_enable"`
-	ProcessCode string `json:"process_code" gorm:"column:process_code"`
+	ProcessCode string `json:"process_code" gorm:"size:255;column:process_code"`
 	// 类型唯一
 	Type string `json:"type" gorm:"unique"`
 }
 
 type CloudbeaverUserCache struct {
 	DMSUserID         string `json:"dms_user_id" gorm:"column:dms_user_id;primaryKey"`
-	DMSFingerprint    string `json:"dms_fingerprint" gorm:"column:dms_fingerprint"`
-	CloudbeaverUserID string `json:"cloudbeaver_user_id" gorm:"column:cloudbeaver_user_id"`
+	DMSFingerprint    string `json:"dms_fingerprint" gorm:"size:255;column:dms_fingerprint"`
+	CloudbeaverUserID string `json:"cloudbeaver_user_id" gorm:"size:255;column:cloudbeaver_user_id"`
 }
 
 type CloudbeaverConnectionCache struct {
 	DMSDBServiceID          string `json:"dms_db_service_id" gorm:"column:dms_db_service_id;primaryKey"`
-	DMSDBServiceFingerprint string `json:"dms_db_service_fingerprint" gorm:"column:dms_db_service_fingerprint"`
-	CloudbeaverConnectionID string `json:"cloudbeaver_connection_id" gorm:"column:cloudbeaver_connection_id"`
+	DMSDBServiceFingerprint string `json:"dms_db_service_fingerprint" gorm:"size:255;column:dms_db_service_fingerprint"`
+	CloudbeaverConnectionID string `json:"cloudbeaver_connection_id" gorm:"size:255;column:cloudbeaver_connection_id"`
 }
 
 type DatabaseSourceService struct {
 	Model
 	Name       string `json:"name" gorm:"size:200;not null;index:project_uid_name,unique" example:""`
-	Source     string `json:"source" gorm:"not null"`
-	Version    string `json:"version" gorm:"not null"`
-	URL        string `json:"url" gorm:"not null"`
-	DbType     string `json:"db_type" gorm:"not null"`
-	ProjectUID string `json:"project_uid" gorm:"column:project_uid;index:project_uid_name,unique"`
+	Source     string `json:"source" gorm:"size:255;not null"`
+	Version    string `json:"version" gorm:"size:255;not null"`
+	URL        string `json:"url" gorm:"size:255;not null"`
+	DbType     string `json:"db_type" gorm:"size:255;not null"`
+	ProjectUID string `json:"project_uid" gorm:"size:32;column:project_uid;index:project_uid_name,unique"`
 	// Cron表达式
-	CronExpress         string          `json:"cron_express" gorm:"column:cron_express; not null"`
+	CronExpress         string          `json:"cron_express" gorm:"size:255;column:cron_express; not null"`
 	LastSyncErr         string          `json:"last_sync_err" gorm:"column:last_sync_err"`
 	LastSyncSuccessTime *time.Time      `json:"last_sync_success_time" gorm:"column:last_sync_success_time"`
 	ExtraParameters     ExtraParameters `json:"extra_parameters" gorm:"TYPE:json"`
