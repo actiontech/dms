f8d76cd3301e271e052bc9a3f756cbe37f796874
yangzhongjiao <yangzhongjiao@actionsky.com>
2024-01-17 06:03:30 +0000

    feat: data export task sql

--- api/dms/service/v1/task.go
+++ api/dms/service/v1/task.go
@@ -78,21 +78,50 @@ type GetDataExportTask struct {
 	DBInfo TaskDBInfo `json:"db_info"`
 	Status string     `json:"status"`
 
+	InstanceName      string `json:"instance_name"`
+	ExecStartTime     string `json:"exec_start_time"`
+	ExecEndTime       string `json:"exec_end_time"`
+	TaskPassRate      int    `json:"task_pass_rate"`
+	TaskScore         int    `json:"task_score"`
+	ExecutionUserName string `json:"execution_user_name"`
 	// SQL审核结果
 	AuditLevel string  `json:"audit_level" enums:"normal,notice,warn,error,"`
 	Score      int32   `json:"score"`
 	PassRate   float64 `json:"pass_rate"`
-
-	// 导出SQL的处理信息
-	ExecSQLRecord []SQLRecord `json:"sql_record"`
+	FileName   string  `json:"file_name"` // 导出文件名
+	// 下载次数，进度等
 }
 
-type SQLRecord struct {
-	ID           string        `json:"uid"`
-	SQL          string        `json:"sql"`
-	CheckResults []CheckResult `json:"results"`
-	// 状态、进度、下载次数等
+// swagger:parameters ListDataExportTaskSQLs
+type ListDataExportTaskSQLsReq struct {
+	// project id
+	// Required: true
+	// in:path
+	ProjectUid string `param:"project_uid" json:"project_uid" validate:"required"`
+	// the maximum count of member to be returned
+	// in:query
+	// Required: true
+	PageSize uint32 `query:"page_size" json:"page_size" validate:"required"`
+	// the offset of members to be returned, default is 0
+	// in:query
+	PageIndex uint32 `query:"page_index" json:"page_index"`
 }
 
-type CheckResult struct {
+// swagger:model ListDataExportTaskSQLsReply
+type ListDataExportTaskSQLsReply struct {
+	Data  []*ListDataExportWorkflow `json:"data"`
+	Total int64                     `json:"total_nums"`
+	// Generic reply
+	base.GenericResp
+}
+
+type ListDataExportTaskSQL struct {
+	ID           string `json:"uid"`
+	SQL          string `json:"sql"`
+	CheckResults string `json:"results"`   // 审核结果
+	FileName     string `json:"file_name"` // 导出文件名
+	AuditResult  string `json:"audit_result"`
+	AuditLevel   string `json:"audit_level"`
+	AuditStatus  string `json:"audit_status"`
+	// 状态
 }
