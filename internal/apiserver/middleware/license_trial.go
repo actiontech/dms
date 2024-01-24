d64e3d740ca3619e819f2f581129b24091362113
taolx0 <taolx25@foxmail.com>
2024-01-24 17:51:10 +0800

    feat:trial version plugin

---
+++ internal/apiserver/middleware/license_trial.go
@@ -0,0 +1,43 @@
+//go:build trial
+// +build trial
+
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/actiontech/dms/internal/dms/biz"
+	"github.com/labstack/echo/v4"
+)
+
+var dbServiceLimit int64 = 20
+
+func licenseAdapter(l *biz.LicenseUsecase) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			path := strings.TrimSuffix(c.Path(), "/")
+			if !(c.Request().Method == http.MethodPost &&
+				path == "/v1/dms/projects/:project_uid/db_services") {
+				return next(c)
+			}
+
+			dbTypeCounts, err := l.DBService.CountDBService(c.Request().Context())
+			if err != nil {
+				return err
+			}
+
+			var total int64
+			for _, count := range dbTypeCounts {
+				total += count.Count
+			}
+
+			if total >= dbServiceLimit {
+				return fmt.Errorf("the number of db services exceeds the limit of %v", dbServiceLimit)
+			}
+
+			return next(c)
+		}
+	}
+}
