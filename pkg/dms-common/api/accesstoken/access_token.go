de72e7fc4eb1183fa06f19eecbbeb6b625429411
bianyucheng <bianyucheng0@gmail.com>
2024-04-02 16:02:21 +0800

    add check access token to dms-common

---
+++ pkg/dms-common/api/accesstoken/access_token.go
@@ -0,0 +1,81 @@
+package accesstoken
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	jwtPkg "github.com/actiontech/dms/pkg/dms-common/api/jwt"
+	"github.com/actiontech/dms/pkg/dms-common/dmsobject"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/labstack/echo/v4"
+)
+
+const AccessTokenLogin = "access_token_login"
+
+func CheckLatestAccessToken(dmsAddress string) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			token, exist, err := GetTokenFromContext(c)
+			if err != nil {
+				return err
+			}
+			if !exist {
+				return next(c)
+			}
+			uid, exist, err := GetUidFromAccessToken(token)
+			if err != nil {
+				return err
+			}
+			if !exist {
+				return next(c)
+			}
+
+			userInfo, err := dmsobject.GetUser(context.TODO(), uid, dmsAddress)
+			if err != nil {
+				return err
+			}
+			if userInfo == nil {
+				return echo.NewHTTPError(http.StatusNotFound, "access token: cannot get user info")
+			}
+
+			if userInfo.AccessTokenInfo.AccessToken != token.Raw {
+				return echo.NewHTTPError(http.StatusUnauthorized, "access token is not latest")
+			}
+
+			return next(c)
+		}
+	}
+}
+
+func GetTokenFromContext(c echo.Context) (token *jwt.Token, exist bool, err error) {
+	user := c.Get("user")
+	// 获取token为空，代表该请求不需要校验token或者是sqle和provision的请求
+	if user == nil {
+		return nil, false, nil
+	}
+	token, ok := user.(*jwt.Token)
+	if !ok {
+		return nil, true, echo.NewHTTPError(http.StatusBadRequest, "failed to convert user from jwt token")
+	}
+
+	return token, true, nil
+}
+
+func GetUidFromAccessToken(token *jwt.Token) (uid string, exist bool, err error) {
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", true, echo.NewHTTPError(http.StatusBadRequest, "failed to convert token claims to jwt")
+	}
+
+	// 如果不存在JWTLoginType字段，代表是账号密码登录获取的token或者是扫描任务的凭证，不进行校验
+	loginType, ok := claims[jwtPkg.JWTLoginType]
+	if !ok {
+		return "", false, nil
+	}
+	if loginType != AccessTokenLogin {
+		return "", true, echo.NewHTTPError(http.StatusUnauthorized, "access token login type is error")
+	}
+	uid = fmt.Sprintf("%v", claims[jwtPkg.JWTUserId])
+	return uid, true, nil
+}
