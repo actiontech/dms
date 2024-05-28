212dc369cb43b45be8fddd35bc98b7851db6ede6
BugsGuru <lwqlwq888@gmail.com>
2024-05-28 16:40:08 +0800

    feat: CbOperationLog regular cleaning

--- internal/dms/biz/cb_operation_log_ee.go
+++ internal/dms/biz/cb_operation_log_ee.go
@@ -4,8 +4,12 @@ package biz
 
 import (
 	"context"
+	"time"
 
 	pkgConst "github.com/actiontech/dms/internal/dms/pkg/constant"
+	"github.com/actiontech/dms/internal/pkg/cloudbeaver"
+	v1Base "github.com/actiontech/dms/pkg/dms-common/api/base/v1"
+	pkgHttp "github.com/actiontech/dms/pkg/dms-common/pkg/http"
 )
 
 func (cu *CbOperationLogUsecase) GetCbOperationLogByID(ctx context.Context, uid string) (*CbOperationLog, error) {
@@ -43,4 +47,48 @@ func (u *CbOperationLogUsecase) ListCbOperationLog(ctx context.Context, option *
 	}
 
 	return operationLogs, count, nil
+}
+
+func (u *CbOperationLogUsecase) DoClean() {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// request SQLE to get cb_operation_logs_expired_hours
+	target, err := u.dmsProxyTargetRepo.GetProxyTargetByName(ctx, cloudbeaver.SQLEProxyName)
+	if err != nil {
+		u.log.Error("CbOperationLogUsecase DoClean GetProxyTargetByName err:", err)
+		return
+	}
+	url := target.URL.String() + "/v1/configurations/system_variables"
+
+	header := map[string]string{
+		"Authorization": pkgHttp.DefaultDMSToken,
+	}
+
+	reply := &struct {
+		v1Base.GenericResp
+		Data struct {
+			CbOperationLogsExpiredHours int `json:"cb_operation_logs_expired_hours"`
+		} `json:"data"`
+	}{}
+	err = pkgHttp.Get(ctx, url, header, nil, reply)
+	if err != nil {
+		u.log.Errorf("failed to clean CB operation log when get expired duration: %v", err)
+		return
+	} else if reply.Code != 0 {
+		u.log.Errorf("failed to clean CB operation log, sqle reply code: %d", reply.Code)
+		return
+	} else if reply.Data.CbOperationLogsExpiredHours <= 0 {
+		u.log.Debugf("got CbOperationLogsExpiredHours: %d", reply.Data.CbOperationLogsExpiredHours)
+		return
+	}
+
+	cleanTime := time.Now().Add(time.Duration(-reply.Data.CbOperationLogsExpiredHours) * time.Hour)
+	rowsAffected, err := u.repo.CleanCbOperationLogOpTimeBefore(ctx, cleanTime)
+	if err != nil {
+		u.log.Errorf("failed to clean CB operation log: %v", err)
+		return
+	}
+	u.log.Infof("CbOperationLog regular cleaned rows: %d before: %s", rowsAffected, cleanTime.Format("2006-01-02 15:04:05"))
+	return
 }
