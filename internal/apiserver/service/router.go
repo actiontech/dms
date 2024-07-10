11ba7d80bea2052ae1034fd3528d349aef70dc01
BugsGuru <lwqlwq888@gmail.com>
2024-07-10 11:12:39 +0800

    define global DBServices list api

--- internal/apiserver/service/router.go
+++ internal/apiserver/service/router.go
@@ -28,6 +28,7 @@ func (s *APIServer) initRouter() error {
 		v1.GET(biz.PersonalizationUrl, s.DMSController.GetStaticLogo)
 		v1.POST("/dms/personalization", s.DMSController.Personalization)
 		v1.GET("/dms/db_services/driver_options", s.DMSController.ListDBServiceDriverOption)
+		v1.GET("/dms/db_services", s.DMSController.ListGlobalDBServices)
 
 		dmsProxyV1 := v1.Group(dmsV1.ProxyRouterGroup)
 		dmsProxyV1.POST("", s.DMSController.RegisterDMSProxyTarget)
