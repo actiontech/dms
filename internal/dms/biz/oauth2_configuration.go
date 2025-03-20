12e52accbd6560119d0191f88ae4045ba6a7bf33
BugsGuru <lwqlwq888@gmail.com>
2025-03-20 17:20:15 +0800

    fix oauth2 login and logout logic
    - dont issue tokens when oauth2 callback without associating user
    - logout oauth2 session when refresh token expired
    - extend the validity period of refreshing token cookies

--- internal/dms/biz/oauth2_configuration.go
+++ internal/dms/biz/oauth2_configuration.go
@@ -124,21 +124,25 @@ func (c CallbackRedirectData) Generate() string {
 }
 
 type ClaimsInfo struct {
-	UserId string  `json:"user_id"` // dms用户ID
-	Iat    float64 `json:"iat"`     // 第三方JWT 签发时间 (Issued At)，Unix 时间戳
-	Exp    float64 `json:"exp"`     // 第三方JWT 过期时间 (Expiration Time)，Unix 时间戳
-	Sub    string  `json:"sub"`     // 第三方JWT 主题 (Subject)，通常是用户ID或唯一标识符
-	Sid    string  `json:"sid"`     // 第三方JWT 会话ID (Session ID)，用于跟踪用户会话
+	UserId     string  `json:"user_id"`     // dms用户ID
+	Iat        float64 `json:"iat"`         // 第三方AccessToken 签发时间 (Issued At)，Unix 时间戳
+	Exp        float64 `json:"exp"`         // 第三方AccessToken 过期时间 (Expiration Time)，Unix 时间戳
+	Sub        string  `json:"sub"`         // 第三方AccessToken 主题 (Subject)，通常是用户ID或唯一标识符
+	Sid        string  `json:"sid"`         // 第三方AccessToken 会话ID (Session ID)，用于跟踪用户会话
+	RefreshIat float64 `json:"refresh_iat"` // 第三方RefreshToken 签发时间 (Issued At)，Unix 时间戳
+	RefreshExp float64 `json:"refresh_exp"` // 第三方RefreshToken 过期时间 (Expiration Time)，Unix 时间戳
 }
 
-func (c ClaimsInfo) DmsToken() (token string, expDura time.Duration, err error) {
-	expDura = time.Duration((c.Exp-c.Iat)*0.9) * time.Second
-	token, err = jwt.GenJwtToken(jwt.WithUserId(c.UserId), jwt.WithExpiredTime(expDura), jwt.WithSub(c.Sub), jwt.WithSid(c.Sid))
+func (c ClaimsInfo) DmsToken() (token string, cookieExp time.Duration, err error) {
+	cookieExp = time.Duration((c.Exp-c.Iat)*0.9) * time.Second
+	token, err = jwt.GenJwtToken(jwt.WithUserId(c.UserId), jwt.WithExpiredTime(cookieExp), jwt.WithSub(c.Sub), jwt.WithSid(c.Sid))
 	return
 }
 
-func (c ClaimsInfo) DmsRefreshToken() (token string, expDura time.Duration, err error) {
-	expDura = time.Duration(c.Exp-c.Iat) * time.Second
-	token, err = jwt.GenRefreshToken(jwt.WithUserId(c.UserId), jwt.WithExpiredTime(expDura), jwt.WithSub(c.Sub), jwt.WithSid(c.Sid))
+func (c ClaimsInfo) DmsRefreshToken() (token string, cookieExp time.Duration, err error) {
+	// cookie有效期更久，和第三方refresh token有效期保持一致
+	// 这样在DmsRefreshToken过期时，cookie仍可获取，用于注销第三方会话
+	cookieExp = time.Duration(c.RefreshExp-c.RefreshIat) * time.Second
+	token, err = jwt.GenRefreshToken(jwt.WithUserId(c.UserId), jwt.WithExpiredTime(time.Duration(c.Exp-c.Iat)*time.Second), jwt.WithSub(c.Sub), jwt.WithSid(c.Sid))
 	return
 }
