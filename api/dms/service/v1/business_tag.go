64f7a70593d3c872b3e6401a7dd908157db7dd60
WinfredLIN <linxiaotao1998@live.com>
2025-04-02 16:22:57 +0800

    feat(api): add business tag related models and requests
    - Create BusinessTag struct with ID and Name fields
    - Add CreateBusinessTagReq, UpdateBusinessTagReq, and ListBusinessTagReq for business tag operations
    - Define ListBusinessTagsReply to respond to list requests

---
+++ api/dms/service/v1/business_tag.go
@@ -0,0 +1,38 @@
+package v1
+
+import base "github.com/actiontech/dms/pkg/dms-common/api/base/v1"
+
+// swagger:model
+type BusinessTag struct {
+	ID uint `json:"id,omitempty"`
+	// 业务标签至少1个字符，最多50个字符
+	Name string `json:"name" validate:"min=1,max=50"`
+}
+
+// swagger:model
+type CreateBusinessTagReq struct {
+	BusinessTag *BusinessTag `json:"business_tag" validate:"required"`
+}
+
+// swagger:model
+type UpdateBusinessTagReq struct {
+	// swagger:ignore
+	BusinessTagID uint         `json:"business_tag_id" validate:"required"`
+	BusinessTag   *BusinessTag `json:"business_tag" validate:"required"`
+}
+
+// swagger:parameters ListBusinessTags
+type ListBusinessTagReq struct {
+	// in:query
+	PageIndex uint32 `query:"page_index" json:"page_index"`
+	// in:query
+	// Required: true
+	PageSize uint32 `query:"page_size" json:"page_size" validate:"required"`
+}
+
+// swagger:model ListBusinessTagsReply
+type ListBusinessTagsReply struct {
+	Data  []*BusinessTag `json:"data"`
+	Total int64          `json:"total_nums"`
+	base.GenericResp
+}
