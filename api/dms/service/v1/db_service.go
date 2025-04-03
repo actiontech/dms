4bd1f83daf4ddb5f9c49e7335af4959a47d0bc31
WinfredLIN <linxiaotao1998@live.com>
2025-04-03 10:40:50 +0800

    feat(api): add new query parameters and mark fields as deprecated
    - Add FilterByEnvironmentTag parameter to ListGlobalDBServicesReq
    - Mark FilterByBusiness and IsFixedBusiness as deprecated
    - Add BusinessTag field to UpdateProject
    - Update ProjectTips struct to be removed due to deprecation
    - Mark project update API as deprecated

--- api/dms/service/v1/db_service.go
+++ api/dms/service/v1/db_service.go
@@ -372,9 +372,13 @@ type ListGlobalDBServicesReq struct {
 	// enum: connect_success,connect_failed
 	// in:query
 	FilterLastConnectionTestStatus *string `query:"filter_last_connection_test_status" json:"filter_last_connection_test_status" validate:"omitempty,oneof=connect_success connect_failed"`
+	// TODO This parameter is deprecated and will be removed soon.
 	// the db service business name
 	// in:query
 	FilterByBusiness string `query:"filter_by_business" json:"filter_by_business"`
+	// filter db services by environment tag
+	// in:query
+	FilterByEnvironmentTag string `query:"filter_by_environment_tag" json:"filter_by_environment_tag"`
 	// the db service host
 	// in:query
 	FilterByHost string `query:"filter_by_host" json:"filter_by_host"`
