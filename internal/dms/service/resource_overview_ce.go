1ad614483a52538ceb04907beaf64a92993444aa
WinfredLIN <linxiaotao1998@live.com>
2025-04-14 18:04:36 +0800

    refactor(dms): remove redundant resource overview CE code
    - Remove resource_overview_ce.go file from internal/dms/biz
    - Update resource_overview_ce.go in internal/dms/service
      - Remove import of biz package
      - Define ErrNotSupportResourceOverview in the service layer

--- internal/dms/service/resource_overview_ce.go
+++ internal/dms/service/resource_overview_ce.go
@@ -4,10 +4,13 @@ package service
 
 import (
 	"context"
+	"errors"
+
 	dmsV1 "github.com/actiontech/dms/api/dms/service/v1"
-	"github.com/actiontech/dms/internal/dms/biz"
 )
 
+var ErrNotSupportResourceOverview = errors.New("resource overview related functions are enterprise version functions")
+
 func (svc *DMSService) GetResourceOverviewStatistics(ctx context.Context, currentUserUid string) (resp *dmsV1.ResourceOverviewStatisticsRes, err error) {
-	return nil, biz.ErrNotSupportResourceOverview
+	return nil, ErrNotSupportResourceOverview
 }
