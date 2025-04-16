022b3122f1faa0c69637d8eae751bb0ad0cbd5a1
WinfredLIN <linxiaotao1998@live.com>
2025-04-16 13:44:12 +0800

    refactor(internal/dms/storage): update function parameter type
    - Change the type of listOptions parameter from ListResourceOverviewOption to biz.ListResourceOverviewOption

--- internal/dms/storage/resource_overview_ce.go
+++ internal/dms/storage/resource_overview_ce.go
@@ -7,6 +7,6 @@ import (
 	"github.com/actiontech/dms/internal/dms/biz"
 )
 
-func (repo *ResourceOverviewRepo) GetResourceList(ctx context.Context, listOptions ListResourceOverviewOption) ([]*biz.ResourceRow, error) {
+func (repo *ResourceOverviewRepo) GetResourceList(ctx context.Context, listOptions biz.ListResourceOverviewOption) ([]*biz.ResourceRow, error) {
 	return nil, nil
 }
