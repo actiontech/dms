f1ed0b43e98592dc1bf2f57ee199aefb2ace00c1
WinfredLIN <linxiaotao1998@live.com>
2025-04-18 10:56:48 +0800

    feat(resource-overview-ce): add csv download functionality for resource overview list
    - Implement DownloadResourceOverviewList method in DMSController
    - Add download route to APIServer router
    - Create DownloadResourceOverviewList method in DMSService (EE version)
    - Update resource_overview_ce.go to include placeholder for CE version

--- internal/apiserver/service/router.go
+++ internal/apiserver/service/router.go
@@ -157,8 +157,9 @@ func (s *APIServer) initRouter() error {
 		resourceOverviewV1 := v1.Group("/dms/resource_overview")
 		resourceOverviewV1.GET("/statistics", s.DMSController.GetResourceOverviewStatistics)
 		resourceOverviewV1.GET("/resource_type_distribution", s.DMSController.GetResourceOverviewResourceTypeDistribution)
-		resourceOverviewV1.GET("/topology",s.DMSController.GetResourceOverviewTopology)
-		resourceOverviewV1.GET("/resource_list",s.DMSController.GetResourceOverviewResourceList)
+		resourceOverviewV1.GET("/topology", s.DMSController.GetResourceOverviewTopology)
+		resourceOverviewV1.GET("/resource_list", s.DMSController.GetResourceOverviewResourceList)
+		resourceOverviewV1.GET("/download", s.DMSController.DownloadResourceOverviewList)
 
 		// oauth2 interface does not require login authentication
 		oauth2V1 := v1.Group("/dms/oauth2")
