c5453be96dfb48de7a75b82c33b2e690480e0800
yangzhongjiao <youyangzhongjiao@actionsky.com>
2025-05-06 07:40:44 +0000

    refactor: Changed variable name from "getwayV1" to "gatewayV1"

--- internal/apiserver/service/router.go
+++ internal/apiserver/service/router.go
@@ -236,15 +236,15 @@ func (s *APIServer) initRouter() error {
 		maskingV1 := v1.Group("/dms/masking")
 		maskingV1.GET("/rules", s.DMSController.ListMaskingRules)
 
-		getwayV1 := v1.Group("/dms/gateways")
+		gatewayV1 := v1.Group("/dms/gateways")
 
-		getwayV1.POST("", s.DMSController.AddGateway)
-		getwayV1.DELETE("/:gateway_id", s.DMSController.DeleteGateway)
-		getwayV1.PUT("/:gateway_id", s.DMSController.UpdateGateway)
-		getwayV1.GET("/:gateway_id", s.DMSController.GetGateway)
-		getwayV1.GET("", s.DMSController.ListGateways)
-		getwayV1.GET("/tips", s.DMSController.GetGatewayTips)
-		getwayV1.PUT("/", s.DMSController.SyncGateways, s.DMSController.DMS.GatewayUsecase.Broadcast())
+		gatewayV1.POST("", s.DMSController.AddGateway)
+		gatewayV1.DELETE("/:gateway_id", s.DMSController.DeleteGateway)
+		gatewayV1.PUT("/:gateway_id", s.DMSController.UpdateGateway)
+		gatewayV1.GET("/:gateway_id", s.DMSController.GetGateway)
+		gatewayV1.GET("", s.DMSController.ListGateways)
+		gatewayV1.GET("/tips", s.DMSController.GetGatewayTips)
+		gatewayV1.PUT("/", s.DMSController.SyncGateways, s.DMSController.DMS.GatewayUsecase.Broadcast())
 
 		if s.CloudbeaverController.CloudbeaverService.CloudbeaverUsecase.IsCloudbeaverConfigured() {
 			cloudbeaverV1 := s.echo.Group(s.CloudbeaverController.CloudbeaverService.CloudbeaverUsecase.GetRootUri())
