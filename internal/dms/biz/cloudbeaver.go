ef66960969e9f0bc576b48ee4d141cfa4cc72d40
BugsGuru <lwqlwq888@gmail.com>
2025-06-24 11:25:39 +0800

    Revert "handle execute task result save duration and count of rows"
    This reverts commit 6f5108060390e06452c5626506921f69f082dc21.

--- internal/dms/biz/cloudbeaver.go
+++ internal/dms/biz/cloudbeaver.go
@@ -471,56 +471,6 @@ func (cu *CloudbeaverUsecase) GraphQLDistributor() echo.MiddlewareFunc {
 
 				// 处理获取SQL执行任务结果请求
 				if params.OperationName == "getSqlExecuteTaskResults" {
-					cloudbeaverResBuf := new(bytes.Buffer)
-					mw := io.MultiWriter(c.Response().Writer, cloudbeaverResBuf)
-					writer := &cloudbeaverResponseWriter{Writer: mw, ResponseWriter: c.Response().Writer}
-					c.Response().Writer = writer
-
-					if err = next(c); err != nil {
-						return err
-					}
-
-					// 从任务ID关联中获取用户ID
-					cbUid, exist := taskIDAssocUid.Load(params.Variables["taskId"])
-					if !exist {
-						return nil
-					}
-					cbUidStr, ok := cbUid.(string)
-					if !ok {
-						return nil
-					}
-
-					// 获取操作日志
-					operationLog, err := cu.cbOperationLogUsecase.GetCbOperationLogByID(ctx, cbUidStr)
-					if err != nil {
-						cu.log.Errorf("get cb operation log by id %s failed: %v", cbUidStr, err)
-						return nil
-					} else {
-						resp := &struct {
-							Data struct {
-								Result *model.SQLExecuteInfo `json:"result"`
-							} `json:"data"`
-						}{}
-						if err := json.Unmarshal(cloudbeaverResBuf.Bytes(), resp); err != nil {
-							cu.log.Errorf("extract task id err: %v", err)
-							return nil
-						}
-						// 更新执行总时间
-						if resp.Data.Result != nil {
-							operationLog.ExecTotalSec = int64(resp.Data.Result.Duration)
-							// 目前每一条SQL只会返回一个结果集，因此只需要记录第一个结果集的行数即可
-							if len(resp.Data.Result.Results) > 0 && resp.Data.Result.Results[0].ResultSet != nil {
-								operationLog.ResultSetRowCount = int64(len(resp.Data.Result.Results[0].ResultSet.Rows))
-							}
-						}
-						// 更新操作日志
-						err := cu.cbOperationLogUsecase.UpdateCbOperationLog(ctx, operationLog)
-						if err != nil {
-							cu.log.Error(err)
-							return nil
-						}
-					}
-
 					// 检查是否需要数据脱敏
 					taskIdAssocMaskingVal, exist := taskIdAssocMasking.LoadAndDelete(params.Variables["taskId"])
 					if !exist {
