71b78ccba87a9662740018dbde0b8ea7f798c6b3
littleniannian <jarviscrick@outlook.com>
2025-09-29 13:59:05 +0800

    fix: remove opts EnableHttps

--- internal/sql_workbench/config/sql_workbench_options.go
+++ internal/sql_workbench/config/sql_workbench_options.go
@@ -1,7 +1,6 @@
 package sql_workbench
 
 type SqlWorkbenchOpts struct {
-	EnableHttps   bool   `yaml:"enable_https"`
 	Host          string `yaml:"host"`
 	Port          string `yaml:"port"`
 	AdminUser     string `yaml:"admin_user"`
