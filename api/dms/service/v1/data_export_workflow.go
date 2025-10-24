ff08390d7806f2945500ea1cb817c7987797c1dd
iwanghc <eternity5701@163.com>
2025-10-24 16:54:43 +0800

    feat: add global data export workflow query interface

--- api/dms/service/v1/data_export_workflow.go
+++ api/dms/service/v1/data_export_workflow.go
@@ -4,12 +4,13 @@ import (
 	"time"
 
 	base "github.com/actiontech/dms/pkg/dms-common/api/base/v1"
+	dmsCommonV1 "github.com/actiontech/dms/pkg/dms-common/api/dms/v1"
 )
 
 // swagger:model
 type AddDataExportWorkflowReq struct {
 	// swagger:ignore
-	ProjectUid string `param:"project_uid" json:"project_uid" validate:"required"`
+	ProjectUid         string             `param:"project_uid" json:"project_uid" validate:"required"`
 	DataExportWorkflow DataExportWorkflow `json:"data_export_workflow"`
 }
 
@@ -76,6 +77,68 @@ type ListDataExportWorkflowsReq struct {
 	FuzzyKeyword string `json:"fuzzy_keyword" query:"fuzzy_keyword"`
 }
 
+// swagger:parameters FilterGlobalDataExportWorkflowReq
+type FilterGlobalDataExportWorkflowReq struct {
+	// filter status list
+	// in:query
+	FilterStatusList []DataExportWorkflowStatus `json:"filter_status_list" query:"filter_status_list"`
+	// filter db service uid
+	// in:query
+	FilterDBServiceUid string `json:"filter_db_service_uid" query:"filter_db_service_uid"`
+	// filter project priority
+	// in:query
+	FilterCurrentStepAssigneeUserId string `json:"filter_current_step_assignee_user_id" query:"filter_current_step_assignee_user_id"`
+	// filter project uids
+	// in:query
+	FilterProjectUids []string `json:"filter_project_uids" query:"filter_project_uids"`
+	// filter project uid
+	// in:query
+	FilterProjectUid string `json:"filter_project_uid" query:"filter_project_uid"`
+	// the maximum count of member to be returned
+	// in:query
+	// Required: true
+	PageSize uint32 `query:"page_size" json:"page_size" validate:"required"`
+	// the offset of members to be returned, default is 0
+	// in:query
+	PageIndex uint32 `query:"page_index" json:"page_index"`
+	// filter the status
+	// in:query
+	FilterByStatus DataExportWorkflowStatus `query:"filter_by_status" json:"filter_by_status"`
+	// filter create user id
+	// in:query
+	FilterByCreateUserUid string `json:"filter_by_create_user_uid" query:"filter_by_create_user_uid"`
+	// filter current assignee user id
+	// in:query
+	FilterCurrentStepAssigneeUserUid string `json:"filter_current_step_assignee_user_uid" query:"filter_current_step_assignee_user_uid"`
+	// filter db_service id
+	// in:query
+	FilterByDBServiceUid string `json:"filter_by_db_service_uid" query:"filter_by_db_service_uid"`
+	// filter fuzzy key word for id/name
+	// in:query
+	FuzzyKeyword string `json:"fuzzy_keyword" query:"fuzzy_keyword"`
+}
+
+// swagger:model GetGlobalDataExportWorkflowsReply
+type GetGlobalDataExportWorkflowsReply struct {
+	Data  []*GlobalDataExportWorkflow `json:"data"`
+	Total int64                       `json:"total_nums"`
+	// Generic reply
+	base.GenericResp
+}
+
+type GlobalDataExportWorkflow struct {
+	WorkflowID   string                   `json:"workflow_uid"`  // 数据导出工单ID
+	WorkflowName string                   `json:"workflow_name"` // 数据导出工单的名称
+	Description  string                   `json:"desc"`          // 数据导出工单的描述
+	Creater      UidWithName              `json:"creater"`       // 数据导出工单的创建人
+	CreatedAt    time.Time                `json:"created_at"`    // 数据导出工单的创建时间
+	Status       DataExportWorkflowStatus `json:"status"`        // 数据导出工单的状态
+
+	CurrentStepAssigneeUsers []UidWithName                           `json:"current_step_assignee_user_list"` // 工单待操作人
+	DBServiceInfos           []*dmsCommonV1.DBServiceUidWithNameInfo `json:"db_service_info,omitempty"`       // 所属数据源信息
+	ProjectInfo              *dmsCommonV1.ProjectInfo                `json:"project_info,omitempty"`          // 所属项目信息
+}
+
 // swagger:model ListDataExportWorkflowsReply
 type ListDataExportWorkflowsReply struct {
 	Data  []*ListDataExportWorkflow `json:"data"`
@@ -95,8 +158,9 @@ type ListDataExportWorkflow struct {
 	ExportedAt   time.Time                `json:"exported_at"`   // 执行数据导出工单的时间
 	Status       DataExportWorkflowStatus `json:"status"`        // 数据导出工单的状态
 
-	CurrentStepAssigneeUsers []UidWithName `json:"current_step_assignee_user_list"` // 工单待操作人
-	CurrentStepType          string        `json:"current_step_type"`
+	CurrentStepAssigneeUsers []UidWithName                           `json:"current_step_assignee_user_list"` // 工单待操作人
+	DBServiceInfos           []*dmsCommonV1.DBServiceUidWithNameInfo `json:"db_service_info,omitempty"`       // 所属数据源信息
+	ProjectInfo              *dmsCommonV1.ProjectInfo                `json:"project_info,omitempty"`          // 所属项目信息
 }
 
 // swagger:enum DataExportWorkflowStatus
@@ -204,8 +268,8 @@ type RejectDataExportWorkflowReq struct {
 	// swagger:ignore
 	ProjectUid string `param:"project_uid" json:"project_uid" validate:"required"`
 	// swagger:ignore
-	DataExportWorkflowUid string `param:"data_export_workflow_uid" json:"data_export_workflow_uid" validate:"required"`
-	Payload RejectDataExportWorkflowPayload `json:"payload" validate:"required"`
+	DataExportWorkflowUid string                          `param:"data_export_workflow_uid" json:"data_export_workflow_uid" validate:"required"`
+	Payload               RejectDataExportWorkflowPayload `json:"payload" validate:"required"`
 }
 
 type CancelDataExportWorkflowPayload struct {
@@ -216,6 +280,6 @@ type CancelDataExportWorkflowPayload struct {
 // swagger:model
 type CancelDataExportWorkflowReq struct {
 	// swagger:ignore
-	ProjectUid string `param:"project_uid" json:"project_uid" validate:"required"`
-	Payload CancelDataExportWorkflowPayload `json:"payload" validate:"required"`
+	ProjectUid string                          `param:"project_uid" json:"project_uid" validate:"required"`
+	Payload    CancelDataExportWorkflowPayload `json:"payload" validate:"required"`
 }
